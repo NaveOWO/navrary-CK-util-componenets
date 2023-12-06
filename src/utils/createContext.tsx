807e039ba718697383b04fc6b8eaa87043e10523
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-12-06 21:27:22 +0900

    feat: 개별 context를 생성하는 유틸함수 작성

---
+++ src/utils/createContext.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+type CreateContextProps<TDefaultValues = {}> = {
+  defaultValue: TDefaultValues;
+  contextName: string;
+};
+
+type ContextReturnType<TDefaultValues> = {
+  [key in `${CreateContextProps<TDefaultValues>["contextName"]}Provider`]: ({
+    children,
+    contextValues,
+  }: React.PropsWithChildren<{ contextValues: TDefaultValues }>) => JSX.Element;
+} & {
+  [key in `${CreateContextProps<TDefaultValues>["contextName"]}UseContext`]: () => TDefaultValues;
+};
+
+export function createContext<TDefaultValues>(
+  props: React.PropsWithChildren<CreateContextProps<TDefaultValues>>
+) {
+  const { defaultValue, contextName, children } = props;
+  const Context = React.createContext<TDefaultValues>(defaultValue);
+
+  function Provider(
+    props: React.PropsWithChildren<{ contextValues: TDefaultValues }>
+  ) {
+    const { contextValues, children } = props;
+    return (
+      <Context.Provider value={contextValues}>{children}</Context.Provider>
+    );
+  }
+
+  Context.displayName = contextName;
+
+  function useContext() {
+    const value = React.useContext(Context);
+
+    if (!value) return;
+
+    return value;
+  }
+
+  return {
+    [`${contextName}Provider`]: Provider,
+    [`${contextName}UseContext`]: useContext,
+  } as ContextReturnType<TDefaultValues>;
+}
