e83fb6e4a123b93bc78b4e7be852f5b76ae0e2d0
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-12-09 16:16:02 +0900

    feat: checkbox export

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,2 +1,3 @@
 export { default as Dialog } from "../components/dialog/Dialog";
 export { default as Select } from "../components/select/Select";
+export { default as CheckBox } from "../components/checkBox/Checkbox";
