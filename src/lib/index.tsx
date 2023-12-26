e3b648a3cfe4739f0053cc12687f00e74d0b3c0c
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-12-27 01:05:44 +0900

    fix: 배포 오류 해결

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,4 +1,4 @@
 export { default as Dialog } from "../components/dialog/Dialog";
 export { default as Select } from "../components/select/Select";
-export { default as CheckBox } from "../components/checkBox/Checkbox";
+export { default as CheckBox } from "../components/checkbox/Checkbox";
 export { default as Switch } from "../components/switch/Switch";
