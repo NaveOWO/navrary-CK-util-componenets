3d3ed359e46d9031cf0aa716e2715b799f9309ce
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2024-01-04 00:03:53 +0900

    Deploy website - based on 8fc27a2fa4fdc6d1372047b2f8c60d905fbaac4d

--- my-website/src/pages/index.jsx
+++ my-website/src/pages/index.jsx
@@ -4,11 +4,11 @@ import { Redirect } from "@docusaurus/router";
 export default function Home() {
   const isBrowser = useIsBrowser();
 
-  // if (isBrowser) {
-  return (
-    <Redirect to={`/navrary-CK-util-componenets/docs/Overview/Introduce`} />
-  );
-  // }
+  if (isBrowser) {
+    return (
+      <Redirect to={`/navrary-CK-util-componenets/docs/Overview/Introduce`} />
+    );
+  }
 
   return null;
 }
